474598996656bbe85fae08d6c997d72206f13063
SabeehAftab <112751194+SabeehAftab@users.noreply.github.com>
2022-10-05 00:06:04 -0700

    Add files via upload

---
+++ task 21/index6.js
@@ -0,0 +1,13 @@
+const Animal = {
+    type: 'Invertebrates', 
+    displayType() {  
+      console.log(this.type);
+    }
+  };
+  
+  const animal1 = Object.create(Animal);
+  animal1.displayType(); 
+  
+  const fish = Object.create(Animal);
+  fish.type = 'Fishes';
+  fish.displayType();
